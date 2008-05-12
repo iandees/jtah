3c816e93ab6f5f50d3b9a0c59d9f4a465db41817
ian.dees <ian.dees@73c24c6b-024d-0410-a31a-8783a6dc2380>
2008-05-12 13:38:17 +0000

    The translation step now adds the <bounds> XML so that the mega-png is correctly generated.

--- javatileshome/src/main/java/com/yellowbkpk/jtah/pipeline/TranslateOSMPipelineNode.java
+++ javatileshome/src/main/java/com/yellowbkpk/jtah/pipeline/TranslateOSMPipelineNode.java
@@ -3,6 +3,7 @@ package com.yellowbkpk.jtah.pipeline;
 import com.ge.medit.util.Conduit;
 
 import com.sun.org.apache.xalan.internal.client.XSLTProcessorApplet;
+import com.yellowbkpk.jtah.Config;
 import com.yellowbkpk.jtah.pipeline.command.PipelineCommand;
 import com.yellowbkpk.jtah.pipeline.command.RenderCommand;
 import com.yellowbkpk.jtah.pipeline.command.TranslateCommand;
@@ -15,13 +16,20 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.UUID;
 
 public class TranslateOSMPipelineNode implements PipelineNode {
@@ -52,20 +60,51 @@ public class TranslateOSMPipelineNode implements PipelineNode {
                 System.err.println(osmFile + " " + osmFile.exists());
                 
                 // Determine which rule set to use
-                File translationFile = new File("osmarender.xsl");
+                File translationFile = new File(Config.OSMARENDER_BASE + "/osmarender.xsl");
                 System.err.println(translationFile + " " + osmFile.exists());
-                File rulesFile = new File("osm-map-features-z12.xml");
+                File rulesFile = new File(Config.OSMARENDER_BASE + "/osm-map-features-z12.xml");
                 System.err.println(rulesFile + " " + rulesFile.exists());
+                
+                // Add bounds and other required information to the rules template
+                StringBuilder b = new StringBuilder();
+                b.append("<bounds maxlat='");
+                NumberFormat f = new DecimalFormat("###.#####");
+                b.append(f.format(comm.getBoundingBox().getMinY()));
+                b.append("' minlon='");
+                b.append(f.format(comm.getBoundingBox().getMinX()));
+                b.append("' minlat='");
+                b.append(f.format(comm.getBoundingBox().getMaxY()));
+                b.append("' maxlon='");
+                b.append(f.format(comm.getBoundingBox().getMaxX()));
+                b.append("' />");
+                String boundsXMLLine = b.toString();
+                
+                File tempRulesFile = new File(UUID.randomUUID() + ".xml");
+                BufferedReader ris = new BufferedReader(new FileReader(rulesFile));
+                BufferedWriter ros = new BufferedWriter(new FileWriter(tempRulesFile));
+                String line;
+                while((line = ris.readLine()) != null) {
+                    // Copy the line to the output
+                    ros.write(line);
+                    ros.write("\n");
+                    
+                    // Look for the bounds marker
+                    if(line.contains("<!--bounds_mkr1-->")) {
+                        ros.write(boundsXMLLine);
+                        ros.write("\n");
+                    }
+                }
+                ros.close();
                 
                 // Use XSLT to transform the file
                 TransformerFactory tFactory = TransformerFactory.newInstance();
                 Transformer transformer = tFactory.newTransformer(new StreamSource(translationFile));
-                transformer.setParameter("osmfile", osmFile.getName());
-                System.err.println("osmfile -> " + osmFile.getCanonicalPath());
+                transformer.setParameter("osmfile", "../" + osmFile.getName());
+                System.err.println("osmfile -> " + "../" + osmFile.getCanonicalPath());
                 
                 File svgFile = new File(UUID.randomUUID() + ".svg");
                 System.err.println(svgFile + " " + svgFile.exists());
-                transformer.transform(new StreamSource(rulesFile), new StreamResult(new FileOutputStream(svgFile)));
+                transformer.transform(new StreamSource(tempRulesFile), new StreamResult(new FileOutputStream(svgFile)));
 
                 // Create a render command
                 PipelineCommand renderCommand = new RenderCommand(comm.getBoundingBox(), svgFile);
