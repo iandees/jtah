934eb8beac6dd51846005a84a67cfd8ff5c3490b
ian.dees <ian.dees@73c24c6b-024d-0410-a31a-8783a6dc2380>
2008-05-13 15:16:54 +0000

    Adding skeleton for PNG splitter step.

---
+++ trunk/javatileshome/src/main/java/com/yellowbkpk/jtah/pipeline/PNGSplitterPipelineNode.java
@@ -0,0 +1,69 @@
+package com.yellowbkpk.jtah.pipeline;
+
+import com.ge.medit.util.Conduit;
+
+import com.yellowbkpk.jtah.pipeline.command.SplitterCommand;
+
+import javax.imageio.ImageIO;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class PNGSplitterPipelineNode implements PipelineNode {
+
+    private Conduit inputPipe;
+    private Conduit outputPipe;
+
+    public PNGSplitterPipelineNode(Conduit inputPipe, Conduit outputPipe) {
+        this.inputPipe = inputPipe;
+        this.outputPipe = outputPipe;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                Object dequeue = inputPipe.dequeue();
+                System.err.println("Splitter dequeued " + dequeue);
+                SplitterCommand comm = (SplitterCommand) dequeue;
+
+                // Get the image file we're splitting
+                File inputImage = comm.getLargeImageFile();
+
+                // Figure out the number of times we have to split the image
+                int tilesAccrossX = comm.getTilesNeededX();
+                int tilesAccrossY = comm.getTilesNeededY();
+
+                // TODO For now these should be 256, but perhaps they could be
+                // smaller later?
+                int targetImgWidth = 256;
+                int targetImgHeight = 256;
+
+                // Create the source buffered image
+                BufferedImage sourceImage = ImageIO.read(inputImage);
+                Graphics sourceGraphics = sourceImage.getGraphics();
+
+                // TODO Could check to see if the requested tiles will fit in
+                // the source image correctly at this point.
+
+                // Create a buffered image the size of the target tile(s)
+                BufferedImage targetImage = new BufferedImage(targetImgWidth, targetImgHeight,
+                        BufferedImage.TYPE_INT_ARGB);
+                Graphics targetGraphics = targetImage.getGraphics();
+
+                // Loop through the chunks of the tile
+                // Determine the relative X and Y coords to pick from
+                // Copy the source image to the target image
+                // Write the target image out to file
+                // Add the new tile to the list of tiles that were split
+                
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
